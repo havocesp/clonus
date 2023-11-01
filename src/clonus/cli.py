6196465d69b6ca83d234bc6de513b2e69e316b46
hex benjamin <hex@hexbenjam.in>
2023-11-01 01:03:08 -0400

    continue CLI development - add `dotenv` for loading GH token - add headers to `cli.py` and `constants.py`

--- src/clonus/cli.py
+++ src/clonus/cli.py
@@ -1,21 +1,80 @@
+"""
+'CLONUS'
+partial github cloner
+(C) 2023 hex benjamin (https://dev.hexbenjam.in)
+
+adapted from 'HR/github-clone'
+(C) 2019-2021 Habib Rehman (https://git.io/HR)
+
+licensed under the APACHE-v2.0 license.
+see './LICENSE' for more information.
+"""
+
 # sourcery skip: docstrings-for-modules
-# import requests
+
+import os
+import requests
+
+from dotenv import load_dotenv
 import click
+from rich.console import Console
 
-# from clonus import __version__
+from clonus import __version__
 
 
-# REQ = requests.Session()
-# REQ.headers.update({"User-Agent": f"hexbenjam.in/ {__version__}"})
+def setup() -> None:
+    """
+    Set up the console and requests session.
+
+    Returns:
+        Tuple[Console, requests.Session]: A tuple containing the console and requests session objects.
+    """
+
+    load_dotenv()
+    console = Console()
+    req_session = requests.Session()
+    req_session.headers.update({"User-Agent": f"hexbenjam.in/ {__version__}"})
+    return console, req_session
+
+
+def error(print_func: callable, message: str) -> None:
+    """
+    Print an error message and exit.
+
+    Args:
+        message (str): The error message to print.
+    """
+
+    print_func(f"[bold red]!!![/red] {message}")
+    exit(1)
 
 
 @click.command()
-def hello() -> None:
-    """
-    Say "wello" to the "Horld".
-    """
-    click.echo("wello, Horld!")
+@click.option("-v", "--version", is_flag=True, help="print version and exit")
+@click.argument("url")
+@click.option(
+    "-t", "--token", default=os.getenv("GITHUB_TOKEN"), help="github oauth token"
+)
+# @click.option("-p", "--path", default=".", help="path to clone to")
+def clone(
+    session: requests.Session,
+    print_func: callable,
+    version: bool,
+    url: str = None,
+    token: str = None,
+) -> None:
+    if version:
+        print(__version__)
+        exit(0)
+
+    if not url:
+        error(print_func, "no url specified.")
+
+    if token:
+        session.headers.update({"Authorization": f"token {token}"})
 
 
 if __name__ == "__main__":
-    hello()
+    _console, _reqsession = setup()
+    _print = _console.print
+    clone(_reqsession, _print)
--- src/clonus/constants.py
+++ src/clonus/constants.py
@@ -1,3 +1,15 @@
+"""
+'CLONUS'
+partial github cloner
+(C) 2023 hex benjamin (https://dev.hexbenjam.in)
+
+adapted from 'HR/github-clone'
+(C) 2019-2021 Habib Rehman (https://git.io/HR)
+
+licensed under the APACHE-v2.0 license.
+see './LICENSE' for more information.
+"""
+
 import re
 
 
